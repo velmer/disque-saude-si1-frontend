9843dc7d795af57e432132ffe59dbba8d61f283e
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-26 15:03:21 -0300

    Deletando o arquivo de values e substituindo por constantes no arquivo já existente constantes.js

--- app/js/constantes.js
+++ app/js/constantes.js
@@ -1,5 +1,11 @@
-app.constant('SITUACAO_PREFEITURA', {
-    NORMAL: 'normal',
-    EXTRA: 'extra',
-    CAOS: 'caos'
-});
+(function () {
+    'use strict';
+
+    app.constant('SITUACAO_PREFEITURA', {
+            NORMAL: 'normal',
+            EXTRA: 'extra',
+            CAOS: 'caos'
+        })
+        .constant('API_BASE_URL', 'http://localhost:5000/api');
+
+})();
