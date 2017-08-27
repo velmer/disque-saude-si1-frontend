9d949c2da80e50c1b70336c47bba2c256630b60e
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-27 17:53:08 -0300

    Criando página crua para exibição das informaçoes da Prefeitura, já sendo possível alterar a situação da mesma

--- app/js/controller/prefeitura.controller.js
+++ app/js/controller/prefeitura.controller.js
@@ -1,8 +1,39 @@
 (function () {
     'use strict';
 
-    app.controller('PrefeituraController', ['PrefeituraService', function (PrefeituraService) {
+    app.controller('PrefeituraController', ['PrefeituraService', 'DialogService', 'toastr', function (PrefeituraService, DialogService, toastr) {
         const self = this;
+
+        self.prefeitura = PrefeituraService.getPrefeitura();
+        self.isEdicao = false;
+
+        /**
+         * Altera a situaçao da prefeitura.
+         */
+        self.alterarSituacao = function () {
+            PrefeituraService.alterarSituacao(self.prefeitura.situacao).then(
+                function (response) {
+                    self.toggleEdicao();
+                    toastr.success('Situação alterada com sucesso!');
+                },function (error) {
+                    DialogService.erro();
+                }
+            );
+        };
+
+        /**
+         * Altera o modo edição.
+         */
+        self.toggleEdicao = function () {
+            self.isEdicao = !self.isEdicao;
+        };
+
+        (function getEficiencia() {
+            PrefeituraService.getEficiencia().then(function (response) {
+                self.eficiencia = response.data;
+            });
+        })();
+
     }]);
 
 })();
