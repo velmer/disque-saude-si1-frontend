5ac5455bb5f57b633a692ddfd9ffb1589284188e
Velmer <velmer.odon@ccc.ufcg.edu.br>
2017-08-28 15:32:49 -0300

    Adicionando verificação para usuário logado na toolbar, a fim de exibir apenas as opções disponíveis caso não haja usuário logado

--- app/js/directive/toolbar.directive.js
+++ app/js/directive/toolbar.directive.js
@@ -1,8 +1,29 @@
 (function () {
     'use strict';
 
-    app.directive('toolbar', function ($state) {
+    app.directive('toolbar', function (AuthService, DialogService, $state) {
         function link(scope, element, attr) {
+
+            scope.usuarioEstaLogado = function () {
+                return AuthService.usuarioEstaLogado();
+            };
+
+            scope.sair = function() {
+                AuthService.sair();
+                $state.go('inicio');
+            };
+
+            scope.mostraDialogDeLogin = function () {
+                const dialogConfig = {
+                    templateUrl: 'view/dialog/login.dialog.html',
+                    controller: 'LoginDialogController',
+                    controllerAs: 'loginCtrl',
+                    clickOutsideToClose: false,
+                    escapeToClose: false
+                };
+
+                return DialogService.personalizado(dialogConfig);
+            };
 
         }
 
